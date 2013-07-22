d76daf66a466e608469245c23ff89e83769e02fe
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-07-21 21:44:09 -0400

    Bump version to 0.0.2

--- package.exs
+++ package.exs
@@ -1,10 +1,11 @@
 Expm.Package.new(
   name: "apprentice",
   description: "Watches files and runs tests(eventually arbitrary code) when those files are modified",
-  version: "0.0.1",
+  version: "0.0.2",
   keywords: ["test", "autotest"],
   maintainers: [[name: "Joe Jackson",
-                 email: "joe.jackson@scimedsolutions.com"]],
+                 email: "joe.jackson@scimedsolutions.com"],
+                [name: "Michael Simpson"],
   repositories: [[github: "joeyjoejoejr/apprentice"]],
   platforms: ["OSX"]
 )
