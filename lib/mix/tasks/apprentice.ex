61585bc36041bb01e3194a0235fa59d00178847e
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-07-21 21:41:29 -0400

    Add workshop file for configuration

--- lib/mix/tasks/apprentice.ex
+++ lib/mix/tasks/apprentice.ex
@@ -8,8 +8,21 @@ defmodule Mix.Tasks.Apprentice do
     workshop.exs at your project's root
     Your apprentice will watch your files and run some code(tests)
   """
-  def run(path) do
+  def run(options) do
+    workshop_path = options[0] || "workshop.exs"
     Mix.Task.run 'app.start'
-    Apprentice.run { ["test", "lib"], "*_test.exs" }
+    Code.require_file(workshop_path)
+    Apprentice.run Apprentice.Workshop.apprentices
+  end
+
+  defmodule Install do
+    @shortdoc "Install the workshop.exs template"
+
+    def run(_) do
+      IO.puts "Copying workshop.exs to you project root"
+      File.cp! "#{__DIR__}/../../../workshop.exs", "workshop.exs"
+      IO.puts "Your apprentice is ready to work. Just edit your workshop file
+      and mix apprentice"
+    end
   end
 end
---
+++ workshop.exs
@@ -0,0 +1,9 @@
+defmodule Apprentice.Workshop do
+  def apprentices do
+    [Apprentice.ExUnit] #return all the apprentices you would like to run
+  end
+
+  def exunit_paths do
+    ["lib", "test"]
+  end
+end
